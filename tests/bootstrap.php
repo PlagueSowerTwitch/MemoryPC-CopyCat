15cdc14085f7750a13f1a6a3174437036f6f5ca9
frederick <frederick.rat@efrei.net>
2025-12-04 23:08:54 +0100

    Correction du Github Action (encore)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,16 +4,23 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+$dotenv = new Dotenv();
+
+// Charger .env si présent, sinon ignorer
+$envFile = dirname(__DIR__).'/.env';
+if (file_exists($envFile)) {
+    $dotenv->bootEnv($envFile);
 }
 
-if (file_exists(dirname(__DIR__) . '/.env.test')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
+// Charger .env.test si présent
+$envTestFile = dirname(__DIR__).'/.env.test';
+if (file_exists($envTestFile)) {
+    $dotenv->bootEnv($envTestFile);
 }
 
-passthru('php '.dirname(__DIR__).'/bin/console doctrine:schema:create --env=test --quiet');
+// Supprimer le schema create si tu n'utilises pas de DB
+// passthru('php '.dirname(__DIR__).'/bin/console doctrine:schema:create --env=test --quiet');
 
-if ($_SERVER['APP_DEBUG']) {
+if (!empty($_SERVER['APP_DEBUG'])) {
     umask(0002);
 }
